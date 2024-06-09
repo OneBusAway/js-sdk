f6f7eae72a8ee159f1e1d347252112495356a9fc
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-06-10 01:30:07 +0300

    chore: add apiKey

--- examples/example.mjs
+++ examples/example.mjs
@@ -2,11 +2,12 @@ import onebusaway from '../dist/index.mjs';
 
 const oba = new onebusaway({
   baseURL: 'https://api.pugetsound.onebusaway.org',
+  apiKey: 'TEST',
 });
 
 async function main() {
-  const response = await oba.agenciesWithCoverage.retrive();
-  console.log(response.status);
+  const response = await oba.agenciesWithCoverage.retrieve();
+  console.log(response);
 }
 
 main();
