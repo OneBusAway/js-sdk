78227aab4bd14b2b3bcd0bb1887f12c86bb10326
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-08-13 00:04:51 +0300

    feat: Add example for retrieving block information

---
+++ examples/block.mjs
@@ -0,0 +1,21 @@
+import onebusaway from '../dist/index.mjs';
+import { loadSettings } from './helpers/load-env.mjs';
+
+// Load settings from .env file, if it exists. If not, we'll use the
+// Puget Sound server URL (which is also the default in the SDK) and
+// the 'TEST' API key.
+const settings = loadSettings({
+  apiKey: 'TEST',
+  baseUrl: 'https://api.pugetsound.onebusaway.org/',
+});
+
+// Create a new instance of the OneBusAway SDK with the settings we loaded.
+const oba = new onebusaway(settings);
+
+async function main() {
+  const blockId = '1_7310845';
+  const response = await oba.block.retrieve(blockId);
+  console.log(response.data.entry.configurations[0].trips[0]);
+}
+
+main();
